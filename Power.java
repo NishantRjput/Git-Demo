3694516c090c357a93fa7c67974c581d5dcc44b1
Nishant <codewithnishant.com>
2021-12-06 19:14:47 +0530

    source file named as Power.java

---
+++ Power.java
@@ -0,0 +1,11 @@
+package pack;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+public class Power
+{
+ public void pow(int n)
+ {
+  System.out.println("Square root of "+n+" is "+Math.pow(n,2));
+ }
+}
